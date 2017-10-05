f3c1f7f3ae3b9ed00fa0dc1409b84384caac8a47
Yaroslav Konoplov <eahome00@gmail.com>
2017-10-05 13:49:47 +0300

    Platform dependent library loading

--- lib/fastsheet.rb
+++ lib/fastsheet.rb
@@ -1,2 +1,21 @@
-require 'fastsheet/libfastsheet'
+require 'fiddle'
+
+case RUBY_PLATFORM
+
+  # Windows
+  when /win32/ then 'dll'
+
+  # OS X
+  when /darwin/ then 'dylib'
+
+  # Linux, BSD
+  else 'so'
+end.tap do |lib_ext|
+  # Load library.
+  lib = Fiddle.dlopen(File.expand_path("../../ext/fastsheet/target/release/libfastsheet.#{lib_ext}", __FILE__))
+
+  # Invoke library entry point.
+  Fiddle::Function.new(lib['Init_libfastsheet'], [], Fiddle::TYPE_VOIDP).call
+end
+
 require 'fastsheet/sheet'
